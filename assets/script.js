3273a264ac56f6a0b69a9ba21da0ed04b6cade8f
Omar Sinan <omaro.hashim@hotmail.com>
2019-03-31 15:32:09 +0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -5,21 +5,29 @@ var len;
 var angleText;
 
 function createFractalTreeBranch(leng) {
+    if (leng < 10) {
+        strokeWeight(1);
+        stroke(0, 150, 0);
+    } else {
+        strokeWeight(2);
+        stroke(100, 70, 30);
+    }
     line(0, 0, 0, -leng);
     translate(0, -leng);
     if (leng > 3) {
         push();
-        rotate(angle);
+        rotate(random(angle - 0.008, angle + 0.008));
         createFractalTreeBranch(leng * 0.7);
         pop();
         push();
-        rotate(-angle);
+        rotate(-random(angle - 0.008, angle + 0.008));
         createFractalTreeBranch(leng * 0.7);
         pop();
     }
 }
 
 function createFractalTree() {
+    strokeWeight(3);
     line(width / 2, height - len, width / 2, height - 2 * len);
     translate(width / 2, height - len);
     createFractalTreeBranch(len);
@@ -31,7 +39,7 @@ function setup() {
     angleSlider.position(90, 30);
     lenSlider = createSlider(0, 200, 100, 0.01);
     lenSlider.position(90, 70);
-    frameRate(10);
+    frameRate(5);
 }
 
 function draw() {
@@ -41,6 +49,7 @@ function draw() {
     fill(0);
     stroke(0);
     textSize(18);
+    strokeWeight(1);
     text("Angle", 30, 45);
     text(angle, 240, 45);
     text("Length", 30, 85);
